2f0ccc6cb8980d2f88080a80e8cf77a627a47415
Mariana Albuquerque <marioliveralbubarris@gmail.com>
2026-09-05 23:17:23 +0000

    fgjhftyied:   main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 def calcular_media(notas):
- if notas:
-  return 0
- return sum(notas) /len(notas)
+   
+    if not notas: 
+        return 0
+    return sum(notas) / len(notas)
 
-def verificar_situação(media):
- if media >= 7.0:
-  return "Aprovado"
- elif 5.0 <= media<7.0:
-  return"Recuperação"
- else:
-  return "Repovado"
+def verificar_situacao(media):  
+    if media >= 7.0:
+        return "Aprovado"
+    elif 5.0 <= media < 7.0:
+        return "Recuperação"
+    else:
+        return "Reprovado"  
 
- def main():
-  print ("----Sistema de Análise de Notas----")
-  try:
-   notas_aluno = [8.0,7.0,9.0,6.5]
-   media = calcular_media(notas_aluno)
-   situação = verificar_situação
+def main():
+    print("----Sistema de Análise de Notas----")
+    try:
+        notas_aluno = [8.0, 7.0, 9.0, 6.5]
+        
+        media = calcular_media(notas_aluno)
+        
+        situacao = verificar_situacao(media) 
+        
+        print(f"Notas: {notas_aluno}")
+        print(f"Média Final: {media:.2f}")
+        print(f"Situação: {situacao}")
+        
+    except Exception as e:  
+        print(f"Ocorreu um erro no processamento: {e}") 
 
-   print(f"Notas:{}
+if __name__ == "__main__":
+    main()
