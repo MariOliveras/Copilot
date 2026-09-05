927911d4802a1dc4fd20307621e393518a8d429e
Mariana Albuquerque <marioliveralbubarris@gmail.com>
2026-09-05 23:17:13 +0000

    sdgf

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-def calcular_media(notas);
- if not notas;
-  return 
+def calcular_media(notas):
+ if notas:
+  return 0
+ return sum(notas) /len(notas)
+
+def verificar_situação(media):
+ if media >= 7.0:
+  return "Aprovado"
+ elif 5.0 <= media<7.0:
+  return"Recuperação"
+ else:
+  return "Repovado"
+
+ def main():
+  print ("----Sistema de Análise de Notas----")
+  try:
+   notas_aluno = [8.0,7.0,9.0,6.5]
+   media = calcular_media(notas_aluno)
+   situação = verificar_situação
+
+   print(f"Notas:{}
